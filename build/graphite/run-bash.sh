719c95c5bd9fe49fb55080b9b07c6d8efc0a2a1d
Angus Liu (RD-TW) <angus_liu@trend.com.tw>
2015-10-12 14:06:43 +0800

    [update] graphite

--- build/graphite/run-bash.sh
+++ build/graphite/run-bash.sh
@@ -3,11 +3,14 @@
 docker run \
  --rm \
  --name=graphite \
+ --privileged=true \
  -p 80:80 \
  -p 2003:2003 \
  -p 2004:2004 \
  -p 7002:7002 \
  -p 2222:22 \
- -v /opt/carbon:/var/lib/carbon \
- graphite
+ -v /opt/carbon:/var/lib/carbon:Z \
+ -ti \
+ graphite \
+ bash -s
 	
