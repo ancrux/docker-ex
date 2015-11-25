28b719803467394930ba952a983f59ed8dfdf5c4
Angus Liu (RD-TW) <angus_liu@trend.com.tw>
2015-11-25 17:16:45 +0800

    [revise] clean up untagged images

--- build/graphite/build.sh
+++ build/graphite/build.sh
@@ -2,3 +2,6 @@
 
 #docker build --rm -t graphite -f graphite.el6.docker .
 docker build -t graphite .
+
+# remove untagged/unused images
+docker rmi -f $(docker images | grep "<none>" | awk "{print \$3}")
