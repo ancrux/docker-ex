a49960fe3c94e2d871a45204260311230d521af9
Angus Liu (RD-TW) <angus_liu@trend.com.tw>
2015-11-25 10:37:27 +0800

    [update] grafana build script and files

--- build/grafana/files/usr/bin/docker-entry.sh
+++ build/grafana/files/usr/bin/docker-entry.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 service ssh start && \
- /usr/sbin/grafana-server --config=/etc/grafana/grafana.ini cfg:default.paths.data=/var/lib/grafana cfg:default.paths.logs=/var/log/grafana
+ /usr/sbin/grafana-server --homepath=/usr/share/grafana --config=/etc/grafana/grafana.ini cfg:default.paths.data=/var/lib/grafana cfg:default.paths.logs=/var/log/grafana
 
