b4c80762f42c5834b546fb4d1f42d70a1dd72a4c
Angus Liu (RD-TW) <angus_liu@trend.com.tw>
2015-11-25 17:13:16 +0800

    [upgrade] grafana to v2.5

--- build/grafana/build.sh
+++ build/grafana/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-#docker build --rm -t grafana -f graphite.el6.docker .
+# docker build --rm -t grafana -f graphite.el6.docker .
 docker build -t grafana .
+
+# remove untagged/unused images
+docker rmi -f $(docker images | grep "<none>" | awk "{print \$3}")
--- build/grafana/run-bash.sh
+++ build/grafana/run-bash.sh
@@ -7,5 +7,5 @@ docker run \
   -p 3000:3000 \
   -p 22002:22 \
   -e "GF_SECURITY_ADMIN_PASSWORD=admin" \
-  grafana
+  grafana bash -s
 
